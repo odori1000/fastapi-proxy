dd77e0f5bd7d11772334d0e811f4a64b13078e34
odori1000 <ohdo7729@gmail.com>
2025-04-28 18:04:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 NAVER_CLIENT_ID = "_MUs39d7RiLsvPw6kEAK"
 NAVER_CLIENT_SECRET = "TO1beTvQkM"
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 @app.get("/blog-search")
 def blog_search(query: str = Query(...), display: int = 5, start: int = 1):
     url = "https://openapi.naver.com/v1/search/blog.json"
